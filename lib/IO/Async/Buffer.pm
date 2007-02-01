cf471be0a82926bafb871779d128cd289a394f47
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2007-02-01 00:45:55 +0000

    Changed (C) declaration at start of lib/IO/Async/Buffer.pm to standard 'GPL or perl'

--- lib/IO/Async/Buffer.pm
+++ lib/IO/Async/Buffer.pm
@@ -1,16 +1,5 @@
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Library General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#  You may distribute under the terms of either the GNU General Public License
+#  or the Artistic License (the same terms as Perl itself)
 #
 #  (C) Paul Evans, 2006 -- leonerd@leonerd.org.uk
 
