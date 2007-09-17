8b56f92c0d529cfba32ffe17a5d3d0d618ee8601
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2007-09-17 21:23:39 +0100

    Cope with versions of Heap::Elem >= 0.80 where the internal representation has been changed

--- lib/IO/Async/TimeQueue.pm
+++ lib/IO/Async/TimeQueue.pm
@@ -162,7 +162,12 @@ package IO::Async::TimeQueue::Elem;
 use strict;
 use base qw( Heap::Elem );
 
-sub new
+# The internal implementation of Heap::Elem changed at 0.80 to be an ARRAY
+# typed object, with a 'val' accessor for the user data, where before it's a
+# plain HASH to be accessed directly. We therefore supply two sets of methods
+# here, and set up the right ones depending on the version.
+
+sub new_HASH
 {
    my $self = shift;
    my $class = ref $self || $self;
@@ -177,24 +182,63 @@ sub new
    return $new;
 }
 
-sub cmp
-{
-   my $self = shift;
-   my $other = shift;
-
-   $self->{time} <=> $other->{time};
-}
-
-sub time 
+sub time_HASH
 {
    my $self = shift;
    return $self->{time};
 }
 
-sub code
+sub code_HASH
 {
    my $self = shift;
    return $self->{code};
+}
+
+sub new_VAL
+{
+   my $self = shift;
+   my $class = ref $self || $self;
+
+   my ( $time, $code ) = @_;
+
+   my $new = $class->SUPER::new(
+      time => $time,
+      code => $code,
+   );
+
+   return $new;
+}
+
+sub time_VAL
+{
+   my $self = shift;
+   return $self->val->{time};
+}
+
+sub code_VAL
+{
+   my $self = shift;
+   return $self->val->{code};
+}
+
+if( $Heap::Elem::VERSION < 0.80 ) {
+  *new  = \&new_HASH;
+  *time = \&time_HASH;
+  *code = \&code_HASH;
+}
+else {
+  *new  = \&new_VAL;
+  *time = \&time_VAL;
+  *code = \&code_VAL;
+}
+
+# This only uses methods so is transparent to HASH or ARRAY
+sub cmp
+{
+   my $self = shift;
+   my $other = shift;
+
+   $self->time <=> $other->time;
 }
 
 1;
