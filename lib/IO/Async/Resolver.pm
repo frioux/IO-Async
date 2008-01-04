f128247db2315f83a2b5f85b2871644f46c716c9
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2008-01-04 02:56:48 +0000

    \n-terminate die strings in Resolver.pm to avoid perl appending " in file at line" message

--- lib/IO/Async/Resolver.pm
+++ lib/IO/Async/Resolver.pm
@@ -222,23 +222,23 @@ taking and returning a list of values exactly as the perl function does:
 
 # Now register the inbuilt methods
 
-register_resolver( 'getpwnam', sub { return getpwnam( $_[0] ) or die "$!" } );
-register_resolver( 'getpwuid', sub { return getpwuid( $_[0] ) or die "$!" } );
+register_resolver( 'getpwnam', sub { return getpwnam( $_[0] ) or die "$!\n" } );
+register_resolver( 'getpwuid', sub { return getpwuid( $_[0] ) or die "$!\n" } );
 
-register_resolver( 'getgrnam', sub { return getgrnam( $_[0] ) or die "$!" } );
-register_resolver( 'getgrgid', sub { return getgrgid( $_[0] ) or die "$!" } );
+register_resolver( 'getgrnam', sub { return getgrnam( $_[0] ) or die "$!\n" } );
+register_resolver( 'getgrgid', sub { return getgrgid( $_[0] ) or die "$!\n" } );
 
-register_resolver( 'getservbyname', sub { return getservbyname( $_[0], $_[1] ) or die "$!" } );
-register_resolver( 'getservbyport', sub { return getservbyport( $_[0], $_[1] ) or die "$!" } );
+register_resolver( 'getservbyname', sub { return getservbyname( $_[0], $_[1] ) or die "$!\n" } );
+register_resolver( 'getservbyport', sub { return getservbyport( $_[0], $_[1] ) or die "$!\n" } );
 
-register_resolver( 'gethostbyname', sub { return gethostbyname( $_[0] ) or die "$!" } );
-register_resolver( 'gethostbyaddr', sub { return gethostbyaddr( $_[0], $_[1] ) or die "$!" } );
+register_resolver( 'gethostbyname', sub { return gethostbyname( $_[0] ) or die "$!\n" } );
+register_resolver( 'gethostbyaddr', sub { return gethostbyaddr( $_[0], $_[1] ) or die "$!\n" } );
 
-register_resolver( 'getnetbyname', sub { return getnetbyname( $_[0] ) or die "$!" } );
-register_resolver( 'getnetbyaddr', sub { return getnetbyaddr( $_[0], $_[1] ) or die "$!" } );
+register_resolver( 'getnetbyname', sub { return getnetbyname( $_[0] ) or die "$!\n" } );
+register_resolver( 'getnetbyaddr', sub { return getnetbyaddr( $_[0], $_[1] ) or die "$!\n" } );
 
-register_resolver( 'getprotobyname',   sub { return getprotobyname( $_[0] ) or die "$!" } );
-register_resolver( 'getprotobynumber', sub { return getprotobynumber( $_[0] ) or die "$!" } );
+register_resolver( 'getprotobyname',   sub { return getprotobyname( $_[0] ) or die "$!\n" } );
+register_resolver( 'getprotobynumber', sub { return getprotobynumber( $_[0] ) or die "$!\n" } );
 
 # Keep perl happy; keep Britain tidy
 1;
