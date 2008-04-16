71121b48936484a11e293944b533e75b5e7d39ef
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2008-04-16 19:44:32 +0100

    Removed code from IO::Async::Buffer, replaced with die instantly

--- lib/IO/Async/Buffer.pm
+++ lib/IO/Async/Buffer.pm
@@ -9,10 +9,6 @@ use strict;
 
 our $VERSION = '0.10';
 
-use base qw( IO::Async::Stream );
-
-use warnings qw();
-
 =head1 NAME
 
 C<IO::Async::Buffer> - backward-compatibility wrapper around
@@ -20,9 +16,8 @@ C<IO::Async::Stream>
 
 =head1 SYNOPSIS
 
-This class should not be used in new code, and is provided for backward
-compatibility for older applications that still use it. It has been renamed
-to C<IO::Async::Stream>. Any application using this class should simply change
+This class should not be used in new code. It has been renamed to
+C<IO::Async::Stream>. Any application using this class should simply change
 
  use IO::Async::Buffer;
 
@@ -38,70 +33,7 @@ The behaviour has not otherwise changed.
 
 =cut
 
-=head1 CONSTRUCTOR
-
-=cut
-
-=head2 $buffer = IO::Async::Buffer->new( %params )
-
-This function wraps a call to C<< IO::Async::Stream->new() >>. It will
-translate the following deprecated options as well:
-
-=over 8
-
-=item on_incoming_data => CODE
-
-This option is deprecated and should not be used in new code. It is maintained
-as a backward-compatibility synonym for C<on_read>.
-
-=back
-
-=cut
-
-sub new
-{
-   my $class = shift;
-   my ( %params ) = @_;
-
-   warnings::warnif 'deprecated',
-      "Use of IO::Async::Buffer is deprecated; use IO::Async::Stream instead";
-
-   if( $params{on_incoming_data} ) {
-      warnings::warnif 'deprecated',
-         "The 'on_incoming_data' callback is deprecated; use 'on_read' instead";
-
-      $params{on_read} = delete $params{on_incoming_data};
-   }
-
-   return $class->SUPER::new( %params );
-}
-
-=head1 DEPRECATED METHODS
-
-This class also provides wrappers for methods that have been renamed between
-C<IO::Async::Buffer> and C<IO::Async::Stream>.
-
-=head2 $buffer->send( $data )
-
-A synonym for C<write()>.
-
-=cut
-
-sub send
-{
-   warnings::warnif 'deprecated',
-      "IO::Async::Buffer->send() is deprecated; use ->write() instead";
-
-   # Use write next time
-   no warnings 'redefine';
-   *send = \&IO::Async::Stream::write;
-
-   # Jump to it now - subsequent calls will go direct
-   goto &IO::Async::Stream::write;
-}
-
-# Keep perl happy; keep Britain tidy
-1;
+die "IO::Async::Buffer is now deprecated. Please use IO::Async::Stream instead\n";
 
 __END__
 
