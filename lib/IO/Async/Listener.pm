cbcbb9532cf1ef09134eba928086a979b13c95e2
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2009-01-22 20:49:33 +0000

    Updated (C) declarations in recently-edited files

--- lib/IO/Async/Listener.pm
+++ lib/IO/Async/Listener.pm
@@ -1,7 +1,7 @@
 #  You may distribute under the terms of either the GNU General Public License
 #  or the Artistic License (the same terms as Perl itself)
 #
-#  (C) Paul Evans, 2008 -- leonerd@leonerd.org.uk
+#  (C) Paul Evans, 2008,2009 -- leonerd@leonerd.org.uk
 
 package IO::Async::Listener;
 
