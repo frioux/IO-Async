13715f0f0192673e82adeedb4506bd39dbced271
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2009-08-20 14:49:08 +0100

    Changed two of the larger TCP listen examples from callback closure into subclass cases, for variety

--- examples/chat-server.pl
+++ examples/chat-server.pl
@@ -5,56 +5,12 @@ use warnings;
 
 use IO::Async::Loop;
 use IO::Async::Stream;
-use IO::Async::Listener;
 
 my $PORT = 12345;
 
 my $loop = IO::Async::Loop->new;
 
-my @clients;
-
-my $listener = IO::Async::Listener->new(
-   on_accept => sub {
-      my $self = shift;
-      my ( $socket ) = @_;
-
-      # $socket is just an IO::Socket reference
-      my $peeraddr = $socket->peerhost . ":" . $socket->peerport;
-
-      # Inform the others
-      $_->write( "$peeraddr joins\n" ) for @clients;
-
-      my $clientstream = IO::Async::Stream->new(
-         handle => $socket,
-
-         on_read => sub {
-            my ( $self, $buffref, $closed ) = @_;
-
-            if( $$buffref =~ s/^(.*\n)// ) {
-               # eat a line from the stream input
-
-               # Reflect it to all but the stream who wrote it
-               $_ == $self or $_->write( "$peeraddr: $1" ) for @clients;
-
-               return 1;
-            }
-
-            return 0;
-         },
-
-         on_closed => sub {
-            my ( $self ) = @_;
-            @clients = grep { $_ != $self } @clients;
-
-            # Inform the others
-            $_->write( "$peeraddr leaves\n" ) for @clients;
-         },
-      );
-
-      $loop->add( $clientstream );
-      push @clients, $clientstream;
-   },
-);
+my $listener = ChatListener->new();
 
 $loop->add( $listener );
 
@@ -67,3 +23,50 @@ $listener->listen(
 );
 
 $loop->loop_forever;
+
+package ChatListener;
+use base qw( IO::Async::Listener );
+
+my @clients;
+
+sub on_accept
+{
+   my $self = shift;
+   my ( $socket ) = @_;
+
+   # $socket is just an IO::Socket reference
+   my $peeraddr = $socket->peerhost . ":" . $socket->peerport;
+
+   # Inform the others
+   $_->write( "$peeraddr joins\n" ) for @clients;
+
+   my $clientstream = IO::Async::Stream->new(
+      handle => $socket,
+
+      on_read => sub {
+         my ( $self, $buffref, $closed ) = @_;
+
+         if( $$buffref =~ s/^(.*\n)// ) {
+            # eat a line from the stream input
+
+            # Reflect it to all but the stream who wrote it
+            $_ == $self or $_->write( "$peeraddr: $1" ) for @clients;
+
+            return 1;
+         }
+
+         return 0;
+      },
+
+      on_closed => sub {
+         my ( $self ) = @_;
+         @clients = grep { $_ != $self } @clients;
+
+         # Inform the others
+         $_->write( "$peeraddr leaves\n" ) for @clients;
+      },
+   );
+
+   $loop->add( $clientstream );
+   push @clients, $clientstream;
+}
--- examples/tcp-proxy.pl
+++ examples/tcp-proxy.pl
@@ -13,65 +13,7 @@ my $CONNECT_PORT = 80;
 
 my $loop = IO::Async::Loop->new;
 
-my $listener = IO::Async::Listener->new(
-   on_accept => sub {
-      my $self = shift;
-      my ( $socket1 ) = @_;
-
-      # $socket is just an IO::Socket reference
-      my $peeraddr = $socket1->peerhost . ":" . $socket1->peerport;
-
-      print STDERR "Accepted new connection from $peeraddr\n";
-
-      $loop->connect(
-         host    => $CONNECT_HOST,
-         service => $CONNECT_PORT,
-
-         on_connected => sub {
-            my ( $socket2 ) = @_;
-
-            # Now we need two Streams, cross-connected.
-            my ( $stream1, $stream2 );
-
-            $stream1 = IO::Async::Stream->new(
-               handle => $socket1,
-
-               on_read => sub {
-                  my ( $self, $buffref, $closed ) = @_;
-                  # Just copy all the data
-                  $stream2->write( $$buffref ); $$buffref = "";
-                  return 0;
-               },
-               on_closed => sub {
-                  $stream2->close_when_empty;
-                  print STDERR "Connection from $peeraddr closed\n";
-               },
-            );
-
-            $stream2 = IO::Async::Stream->new(
-               handle => $socket2,
-
-               on_read => sub {
-                  my ( $self, $buffref, $closed ) = @_;
-                  # Just copy all the data
-                  $stream1->write( $$buffref ); $$buffref = "";
-                  return 0;
-               },
-               on_closed => sub {
-                  $stream1->close_when_empty;
-                  print STDERR "Connection to $CONNECT_HOST:$CONNECT_PORT closed\n";
-               },
-            );
-
-            $loop->add( $stream1 );
-            $loop->add( $stream2 );
-         },
-
-         on_resolve_error => sub { print STDERR "Cannot resolve - $_[0]\n"; },
-         on_connect_error => sub { print STDERR "Cannot connect\n"; },
-      );
-   },
-);
+my $listener = ProxyListener->new();
 
 $loop->add( $listener );
 
@@ -84,3 +26,65 @@ $listener->listen(
 );
 
 $loop->loop_forever;
+
+package ProxyListener;
+use base qw( IO::Async::Listener );
+
+sub on_accept
+{
+   my $self = shift;
+   my ( $socket1 ) = @_;
+
+   # $socket is just an IO::Socket reference
+   my $peeraddr = $socket1->peerhost . ":" . $socket1->peerport;
+
+   print STDERR "Accepted new connection from $peeraddr\n";
+
+   $loop->connect(
+      host    => $CONNECT_HOST,
+      service => $CONNECT_PORT,
+
+      on_connected => sub {
+         my ( $socket2 ) = @_;
+
+         # Now we need two Streams, cross-connected.
+         my ( $stream1, $stream2 );
+
+         $stream1 = IO::Async::Stream->new(
+            handle => $socket1,
+
+            on_read => sub {
+               my ( $self, $buffref, $closed ) = @_;
+               # Just copy all the data
+               $stream2->write( $$buffref ); $$buffref = "";
+               return 0;
+            },
+            on_closed => sub {
+               $stream2->close_when_empty;
+               print STDERR "Connection from $peeraddr closed\n";
+            },
+         );
+
+         $stream2 = IO::Async::Stream->new(
+            handle => $socket2,
+
+            on_read => sub {
+               my ( $self, $buffref, $closed ) = @_;
+               # Just copy all the data
+               $stream1->write( $$buffref ); $$buffref = "";
+               return 0;
+            },
+            on_closed => sub {
+               $stream1->close_when_empty;
+               print STDERR "Connection to $CONNECT_HOST:$CONNECT_PORT closed\n";
+            },
+         );
+
+         $loop->add( $stream1 );
+         $loop->add( $stream2 );
+      },
+
+      on_resolve_error => sub { print STDERR "Cannot resolve - $_[0]\n"; },
+      on_connect_error => sub { print STDERR "Cannot connect\n"; },
+   );
+}
