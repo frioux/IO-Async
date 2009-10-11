9b3bf7a98f03fd03b58f7488a788947608629c65
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2009-10-11 19:39:43 +0100

    Also moved 'signal' and 'idle' tests

--- lib/IO/Async/LoopTests.pm
+++ lib/IO/Async/LoopTests.pm
@@ -14,11 +14,13 @@ our @EXPORT = qw(
 );
 
 use Test::More;
+use Test::Exception;
 use Test::Refcount;
 
 use IO::Async::Test;
 
 use POSIX qw( SIGTERM WIFEXITED WEXITSTATUS WIFSIGNALED WTERMSIG );
+use Time::HiRes qw( time );
 
 our $VERSION = '0.23';
 
@@ -79,6 +81,139 @@ The following test suite names exist, to be passed as a name in the C<@tests>
 argument to C<run_tests>:
 
 =cut
+
+=head2 signal
+
+Tests the Loop's ability to watch POSIX signals
+
+=cut
+
+use constant count_tests_signal => 13;
+sub run_tests_signal
+{
+   my ( $class ) = @_;
+
+   my $loop = $class->new();
+
+   is_oneref( $loop, '$loop has refcount 1 initally' );
+
+   my $caught;
+
+   $loop->watch_signal( TERM => sub { $caught = 1 } );
+
+   is_oneref( $loop, '$loop has refcount 1 after watch_signal()' );
+
+   $loop->loop_once( 0.1 );
+
+   is( $caught, undef, '$caught idling' );
+
+   kill SIGTERM, $$;
+
+   $loop->loop_once( 0.1 );
+
+   is( $caught, 1, '$caught after raise' );
+
+   is_oneref( $loop, '$loop has refcount 1 before unwatch_signal()' );
+
+   $loop->unwatch_signal( 'TERM' );
+
+   is_oneref( $loop, '$loop has refcount 1 after unwatch_signal()' );
+
+   my ( $cA, $cB );
+
+   my $idA = $loop->attach_signal( TERM => sub { $cA = 1 } );
+   my $idB = $loop->attach_signal( TERM => sub { $cB = 1 } );
+
+   is_oneref( $loop, '$loop has refcount 1 after 2 * attach_signal()' );
+
+   kill SIGTERM, $$;
+
+   $loop->loop_once( 0.1 );
+
+   is( $cA, 1, '$cA after raise' );
+   is( $cB, 1, '$cB after raise' );
+
+   $loop->detach_signal( 'TERM', $idA );
+
+   undef $cA;
+   undef $cB;
+
+   kill SIGTERM, $$;
+
+   $loop->loop_once( 0.1 );
+
+   is( $cA, undef, '$cA after raise' );
+   is( $cB, 1,     '$cB after raise' );
+
+   $loop->detach_signal( 'TERM', $idB );
+
+   dies_ok( sub { $loop->attach_signal( 'this signal name does not exist', sub {} ) },
+            'Bad signal name fails' );
+
+   is_oneref( $loop, '$loop has refcount 1 finally' );
+}
+
+=head2 idle
+
+Tests the Loop's support for idle handlers
+
+=cut
+
+use constant count_tests_idle => 9;
+sub run_tests_idle
+{
+   my ( $class ) = @_;
+
+   my $loop = $class->new();
+
+   my $called = 0;
+
+   my $id = $loop->watch_idle( when => 'later', code => sub { $called++ } );
+
+   ok( defined $id, 'idle watcher id is defined' );
+
+   is( $called, 0, 'deferred sub not yet invoked' );
+
+   my ( $now, $took );
+
+   $now = time;
+   $loop->loop_once( 3 );
+   $took = time - $now;
+
+   is( $called, 1, 'deferred sub called after loop_once' );
+
+   cmp_ok( $took, '<', 1, 'loop_once(3) with deferred sub takes no more than 1 second' );
+
+   $loop->watch_idle( when => 'later', code => sub {
+      $loop->watch_idle( when => 'later', code => sub { $called++ } )
+   } );
+
+   $loop->loop_once( 1 );
+
+   is( $called, 1, 'inner deferral not yet invoked' );
+
+   $loop->loop_once( 1 );
+
+   is( $called, 2, 'inner deferral now invoked' );
+
+   $id = $loop->watch_idle( when => 'later', code => sub { $called = 20 } );
+
+   $loop->unwatch_idle( $id );
+
+   $now = time;
+   $loop->loop_once( 1 );
+   $took = time - $now;
+
+   is( $called, 2, 'unwatched deferral not called' );
+
+   cmp_ok( $took, '>', '0.9', 'loop_once(1) with unwatched deferral takes more than 0.9 seconds' );
+
+   $loop->later( sub { $called++ } );
+
+   $loop->loop_once( 1 );
+
+   is( $called, 3, '$loop->later() shortcut works' );
+}
 
 =head2 child
 
