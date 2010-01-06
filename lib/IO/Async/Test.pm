756ddadd6857536ae72632a8b61025054f36b522
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2010-01-06 19:58:56 +0000

    use Exporter 'import' instead of @ISAing it

--- lib/IO/Async/Test.pm
+++ lib/IO/Async/Test.pm
@@ -1,7 +1,7 @@
 #  You may distribute under the terms of either the GNU General Public License
 #  or the Artistic License (the same terms as Perl itself)
 #
-#  (C) Paul Evans, 2007,2008 -- leonerd@leonerd.org.uk
+#  (C) Paul Evans, 2007-2010 -- leonerd@leonerd.org.uk
 
 package IO::Async::Test;
 
@@ -10,8 +10,7 @@ use warnings;
 
 our $VERSION = '0.27';
 
-use Exporter;
-our @ISA = qw( Exporter );
+use Exporter 'import';
 our @EXPORT = qw(
    testing_loop
    wait_for
