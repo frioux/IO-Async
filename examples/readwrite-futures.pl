541b497164d5fe9eb587bcd50a0d8edfe0843299
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2013-06-11 18:57:37 +0100

    Added a simple example of Stream reading/writing via Futures

---
+++ examples/readwrite-futures.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use IO::Async::Loop;
+use IO::Async::Stream;
+
+my $loop = IO::Async::Loop->new;
+
+$loop->add( my $stdin  = IO::Async::Stream->new_for_stdin( on_read => sub { 0 } ) );
+$loop->add( my $stdout = IO::Async::Stream->new_for_stdout );
+
+$stdout->write( sub {
+   return undef if $stdin->is_read_eof;
+   return $stdin->read_atmost( 64 * 1024 );
+}, on_flush => sub { $loop->stop } );
+
+$loop->run;
