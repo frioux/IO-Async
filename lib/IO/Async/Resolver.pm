956f9ffb8e6a372c22209e3916ab9e9f2ab98bb2
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2013-10-10 18:13:29 +0100

    Declare minimum Socket version of 2.007, for MSWin32

--- lib/IO/Async/Resolver.pm
+++ lib/IO/Async/Resolver.pm
@@ -11,7 +11,8 @@ use base qw( IO::Async::Function );
 
 our $VERSION = '0.60_002';
 
-use Socket 1.93 qw(
+# Socket 2.006 fails to getaddrinfo() AI_NUMERICHOST properly on MSWin32
+use Socket 2.007 qw(
    AI_NUMERICHOST AI_PASSIVE
    NI_NUMERICHOST NI_NUMERICSERV NI_DGRAM
    EAI_NONAME
